57345ac2457c834fdb37323a93edd5ee73fc0e10
Stian have <stianhave@hotmail.com>
2020-05-11 00:33:31 +0200

    added features and visualization

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let MyComponents;
+let MyFeatures;
+
+async function getFeatures() {
+  await fetch("features.json")
+    .then((x) => x.json())
+    .then((requirements) => {
+      MyFeatures = requirements;
+      CreateFeatureRequirements("Landing page");
+    });
+}
+
 async function GetComponents() {
   await fetch("components.json")
     .then((x) => x.json())
@@ -6,6 +17,8 @@ async function GetComponents() {
       console.log(components);
       MyComponents = components;
       LogComponents(components.Backend_Component, "UI Component");
+      CreateElement(components.Backend_Module);
+      GetRequirements(components.Backend_Module);
     });
 }
 
@@ -15,4 +28,70 @@ async function LogComponents(compontent, compontentName) {
   console.log(`Component time ${compontent.time}`);
   console.log(`Component requirements ${compontent.requirements}`);
 }
+
+function CreateElement(component) {
+  let container = document.createElement("div");
+  container.classList.add("element-container");
+
+  let MainComponent = document.createElement("div");
+  MainComponent.classList.add("main-component");
+  MainComponent.innerText = component.name;
+
+  container.appendChild(MainComponent);
+
+  component.requirements.forEach((element) => {
+    console.log(`added ${element.name}`);
+    let RequiredComponent = document.createElement("div");
+    RequiredComponent.innerText = `Required: ${element.amount} : ${element.name}`;
+    container.appendChild(RequiredComponent);
+  });
+
+  document.querySelector("main").appendChild(container);
+}
+
+function GetRequirements(component) {
+  const list = [];
+  component.requirements.forEach((r) => {
+    for (let i = 0; i < r.amount; i++) {
+      list.push(r.name);
+      console.log(r.name);
+    }
+  });
+  return list;
+}
+
+function GetFeatureRequirements() {
+  MyFeatures.Landing_Page.forEach((component) => {
+    console.log(
+      `Landing page component: ${component.amount}: ${component.name}`
+    );
+  });
+}
+
+function CreateFeatureRequirements(name) {
+  let featureDiv = document.createElement("div");
+  featureDiv.classList.add("feature");
+
+  let featureName = document.createElement("h1");
+  featureName.innerText = name;
+  featureDiv.appendChild(featureName);
+  MyFeatures.Landing_Page.forEach((component) => {
+    let requiredComponent = document.createElement("div");
+    requiredComponent.classList.add("required-feature-component");
+    let componentName = document.createElement("p");
+    componentName.innerText = component.name;
+
+    let componentAmount = document.createElement("p");
+    componentAmount.innerText = component.amount;
+
+    requiredComponent.appendChild(componentName);
+    requiredComponent.appendChild(componentAmount);
+
+    featureDiv.appendChild(requiredComponent);
+  });
+
+  document.querySelector("main").appendChild(featureDiv);
+}
+
 GetComponents();
+getFeatures();
