3f7a6a49bc1ecbf05d8be0fa3721c8903ac4a5b9
Stian have <stianhave@hotmail.com>
2020-05-11 19:34:58 +0200

    fixed recursion, calculation now correct

--- main.js
+++ main.js
@@ -60,77 +60,83 @@ function GetFeatureRequirements() {
   MyFeatures.Landing_Page.forEach((component) => {});
 }
 
-function CreateFeatureRequirements(feature, name) {
-  let featureDiv = document.createElement("div");
-  featureDiv.classList.add("feature");
+let requirementsList = [];
+let requirementsAmount = [];
 
-  let featureName = document.createElement("h1");
-  featureName.innerText = name;
-  featureDiv.appendChild(featureName);
-
-  feature.forEach((component) => {
-    let requiredComponent = document.createElement("div");
-    requiredComponent.classList.add("feature-required-component");
-    let componentName = document.createElement("p");
-    componentName.innerText = component.name;
-
-    let componentAmount = document.createElement("p");
-    componentAmount.innerText = component.amount;
-
-    let dash = document.createElement("p");
-    dash.innerText = ":";
-
-    requiredComponent.appendChild(componentName);
-    requiredComponent.appendChild(dash);
-    requiredComponent.appendChild(componentAmount);
-
-    featureDiv.appendChild(requiredComponent);
-
-    /* Find components required component */
-    MyComponents[component.name].requirements.forEach((requiredComponent) => {
-      let ComponentsRequiredComponent = document.createElement("p");
-      ComponentsRequiredComponent.classList.add("sub-component");
-      ComponentsRequiredComponent.innerText = `${component.name} requires ${
-        requiredComponent.amount * component.amount
-      } ${requiredComponent.name}`;
-      featureDiv.appendChild(ComponentsRequiredComponent);
-
-      /*Required-Feature-Component's required component, required component */
-
-      MyComponents[requiredComponent.name].requirements.forEach((rc) => {
-        let rfcrcrc = document.createElement("div");
-        rfcrcrc.classList.add("sub-sub-requirements");
-        let rfcrcrComponent = document.createElement("p");
-        rfcrcrComponent.innerText = `${requiredComponent.name} requires ${
-          requiredComponent.amount * rc.amount
-        } ${rc.name}`;
-        rfcrcrComponent.classList.add("sub-sub-component");
-        rfcrcrc.appendChild(rfcrcrComponent);
-        featureDiv.appendChild(rfcrcrc);
-
-        /*Required-Feature-Component's required component's required components, required component */
-        try {
-          MyComponents[rc.name].requirements.forEach((rc2) => {
-            let rfcrcrc2 = document.createElement("div");
-            rfcrcrc2.classList.add("sub-sub-sub-requirements");
-            let rfcrcrComponent2 = document.createElement("p");
-            rfcrcrComponent.innerText = `${rc.name} requires ${
-              rc.amount * rc2.amount
-            } ${rc2.name}`;
-            rfcrcrComponent2.classList.add("sub-sub-sub-component");
-            rfcrcrc2.appendChild(rfcrcrComponent2);
-            featureDiv.appendChild(rfcrcrc2);
-            console.log(`${rc.name} requires ${rc2.name}`);
-          });
-        } catch (error) {
-          console.log(error);
-          console.log(`${MyComponents[rc.name].name} was faulty`);
-        }
-      });
+function ComponentRequirements(_component, amount) {
+  /* parse from feature.component to components.component */
+  component = MyComponents[_component.name];
+  for (var i = 0; i < amount; i++) {
+    requirementsList.push(component.name);
+  }
+  /* Check if component has requirements */
+  if (ComponentHasRequirements(component.requirements)) {
+    component.requirements.forEach((requirement) => {
+      if (amount > 1) {
+        ComponentRequirements(requirement, requirement.amount * amount);
+      } else {
+        ComponentRequirements(requirement, requirement.amount);
+      }
     });
-  });
+  } else {
+    console.log(`${component.name} has no requirements`);
+  }
+}
 
-  document.querySelector("main").appendChild(featureDiv);
+function ComponentHasRequirements(requirements) {
+  if (requirements.length > 0) {
+    return true;
+  }
+  return false;
+}
+let counts = {};
+function CreateFeatureRequirements(feature, name) {
+  requirementsList = [];
+  feature.forEach((Requirements) => {
+    requirementsAmount = [];
+    requirementsList.push(Requirements.name);
+    ComponentRequirements(
+      MyComponents[Requirements.name],
+      MyComponents[Requirements.name].amount
+    );
+    requirementsList.sort();
+
+    /* */
+    /* */
+  });
+  counts = {};
+  requirementsList.forEach(function (x) {
+    counts[x] = (counts[x] || 0) + 1;
+  });
+  console.log(counts);
+  CreateFeatureHtml(name);
+}
+
+function CreateFeatureHtml(name) {
+  const featureContainer = document.createElement("div");
+  featureContainer.classList.add("feature");
+
+  featureHeader = document.createElement("h1");
+  featureHeader.innerText = name;
+  featureContainer.appendChild(featureHeader);
+
+  const componentContainer = document.createElement("div");
+  // counts.forEach((com) => {
+  //   let component = document.createElement("p");
+  //   component.innerText = `component ${com}`;
+  //   componentContainer.appendChild(component);
+  // });
+
+  var propValue;
+  for (var propName in counts) {
+    propValue = counts[propName];
+    let component = document.createElement("p");
+    component.innerText = `${propValue} : ${propName}`;
+    componentContainer.appendChild(component);
+  }
+
+  featureContainer.appendChild(componentContainer);
+  document.querySelector("main").appendChild(featureContainer);
 }
 
 GetComponents();
